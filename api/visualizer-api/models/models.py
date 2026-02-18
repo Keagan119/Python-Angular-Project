65ff3f2a861f1aebb115033178c9c042492ce4fb
keagan.williams <keagan.williams@s4.co.za>
2026-02-18 18:36:23 +0200

    Add initial db models

---
+++ api/visualizer-api/models/models.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column, Integer, Float, String, Date
+from database.database import Base
+
+class Expense(Base):
+    __tablename__ = "expenses"
+
+    id = Column(Integer, primary_key=True, index=True)
+    amount = Column(Float, nullable=False)
+    category = Column(String, nullable=False)
+    description = Column(String, nullable=True)
+    date = Column(Date, nullable=False)
