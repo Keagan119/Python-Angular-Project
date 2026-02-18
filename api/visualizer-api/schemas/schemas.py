ebdb41b9c825fb69387fc5611ed09e2a40f1326c
keagan.williams <keagan.williams@s4.co.za>
2026-02-18 18:37:46 +0200

    Add pydantic validation models

---
+++ api/visualizer-api/schemas/schemas.py
@@ -0,0 +1,14 @@
+from pydantic import BaseModel
+from datetime import date
+
+class ExpenseCreate(BaseModel):
+    amount: float
+    category: str
+    description: str | None = None
+    date: date
+
+class ExpenseResponse(ExpenseCreate):
+    id: int
+
+    class Config:
+        from_attributes = True
