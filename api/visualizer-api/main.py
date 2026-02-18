85f6f9e7352eb4f2a844c0e725584b0a162e7e4b
keagan.williams <keagan.williams@s4.co.za>
2026-02-18 18:36:53 +0200

    Implement cors, routing and db initialization

--- api/visualizer-api/main.py
+++ api/visualizer-api/main.py
@@ -1,7 +1,29 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from database.database import engine, Base
+from routes.routes import router
 
-app = FastAPI()
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI(title="Finance Analytics API")
+
+
+origins = [
+    "http://localhost:4200", 
+    "http://127.0.0.1:4200",
+   
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,    
+    allow_credentials=True,      
+    allow_methods=["*"],         
+    allow_headers=["*"],         
+)
+
+app.include_router(router)
 
 @app.get("/")
 def root():
-    return {"message": "Hello World"}
+    return {"message": "Finance API is running"}
