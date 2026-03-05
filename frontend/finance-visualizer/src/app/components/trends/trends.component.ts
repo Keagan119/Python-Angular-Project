89be4b97bc2053ecd876fa3301b308f76383fd29
keagan.williams <keagan.williams@s4.co.za>
2026-03-05 16:51:38 +0200

    Add trend component

---
+++ frontend/finance-visualizer/src/app/components/trends/trends.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-trends',
+  templateUrl: './trends.component.html',
+  styleUrls: ['./trends.component.css'],
+  standalone: false
+})
+export class TrendsComponent {}
