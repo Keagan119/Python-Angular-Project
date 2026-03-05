c2df87231a14f5d6e777a40296c8d6842632ea1d
keagan.williams <keagan.williams@s4.co.za>
2026-03-05 16:42:34 +0200

    Adjust DB models

--- api/visualizer-api/models/models.py
+++ api/visualizer-api/models/models.py
@@ -1,11 +1,26 @@
-from sqlalchemy import Column, Integer, Float, String, Date
+from sqlalchemy import Column, Integer, Float, String, Date, Boolean, ForeignKey
 from database.database import Base
 
-class Expense(Base):
-    __tablename__ = "expenses"
+
+class Transaction(Base):
+    __tablename__ = "transactions"
 
     id = Column(Integer, primary_key=True, index=True)
     amount = Column(Float, nullable=False)
     category = Column(String, nullable=False)
     description = Column(String, nullable=True)
     date = Column(Date, nullable=False)
+    user_id = Column(Integer, ForeignKey('users.id'), nullable=True, index=True)
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    is_active = Column(Boolean, default=True)
+    name = Column(String, nullable=True)
+    surname = Column(String, nullable=True)
+    occupation = Column(String, nullable=True)
+    monthly_income = Column(Float, nullable=True)
